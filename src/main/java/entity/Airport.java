3edd88d8dcbf4206238aa9e0897e7357087f06a2
github_frog2361 <ivashkovalera1@gmail.com>
2023-01-27 23:49:26 +0300

    реализовал проверку статуса пользователя

--- src/main/java/entity/Airport.java
+++ src/main/java/entity/Airport.java
@@ -5,9 +5,10 @@ import valid.Valid;
 import java.time.LocalDateTime;
 
 public class Airport {
-    public void passPersonToAirplane(LocalDateTime personCameAt, LocalDateTime boardingTimeWillStartAt, LocalDateTime airplaneWillFlyAt) {
+    public void passPersonToAirplane(LocalDateTime personCameAt, LocalDateTime boardingTimeWillStartAt, LocalDateTime airplaneWillFlyAt, UserStatus userStatus) {
         Valid.checkIfPersonComeLate(personCameAt, airplaneWillFlyAt);
         Valid.checkIfPersonEarly(personCameAt, boardingTimeWillStartAt);
+        Valid.checkIfPersonBlocked(userStatus);
         System.out.println("Человек успешно сел на борт");
     }
 }
